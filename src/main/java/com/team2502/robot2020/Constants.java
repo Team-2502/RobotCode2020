e55c13f89882a416d33f038518d7998265c6d4fd
Ishan Shetty <ishan.h.shetty@gmail.com>
2020-01-08 15:35:41 -0600

    Added more temporary constants

--- src/main/java/com/team2502/robot2020/Constants.java
+++ src/main/java/com/team2502/robot2020/Constants.java
@@ -8,6 +8,7 @@
 package com.team2502.robot2020;
 
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -50,6 +51,32 @@ public final class Constants {
             // TODO set correct port
             public static final SPI.Port kNavXPort = SPI.Port.kMXP;
             public static final boolean kGyroReversed = false;
+
+            // These are example values only - DO NOT USE THESE FOR YOUR OWN ROBOT!
+            // These characterization values MUST be determined either experimentally or theoretically
+            // for *your* robot's drive.
+            // The Robot Characterization Toolsuite provides a convenient tool for obtaining these
+            // values for your robot.
+            public static final double ksVolts = 0.22;
+            public static final double kvVoltSecondsPerMeter = 1.98;
+            public static final double kaVoltSecondsSquaredPerMeter = 0.2;
+
+            public static final double kTrackwidthMeters = 0.69;
+            public static final DifferentialDriveKinematics kDriveKinematics =
+                    new DifferentialDriveKinematics(kTrackwidthMeters);
+
+            // Example value only - as above, this must be tuned for your drive!
+            public static final double kPDriveVel = 8.5;
+        }
+
+        public static final class Auto {
+
+            public static final double kMaxSpeedMetersPerSecond = 3;
+            public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+
+            // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
+            public static final double kRamseteB = 2;
+            public static final double kRamseteZeta = 0.7;
         }
     }
 }
