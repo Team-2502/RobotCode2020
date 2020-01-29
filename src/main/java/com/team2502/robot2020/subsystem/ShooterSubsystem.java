e8fa92c497cf33a8d64c8f5fcb223e05fa6a699e
AdityaK29 <aditya29@icloud.com>
2020-01-28 23:31:16 -0600

    Added Motor Following and basic stop methods. Also added comment with link to all CANSparkMax methods at top.

--- src/main/java/com/team2502/robot2020/subsystem/ShooterSubsystem.java
+++ src/main/java/com/team2502/robot2020/subsystem/ShooterSubsystem.java
@@ -4,6 +4,9 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+//* CANSparkMax methods, http://www.revrobotics.com/content/sw/max/sw-docs/java/com/revrobotics/CANSparkMax.html
+
+
 public class ShooterSubsystem extends SubsystemBase {
 
     private final CANSparkMax shootRight;
@@ -15,21 +18,27 @@ public class ShooterSubsystem extends SubsystemBase {
         shootRight = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
 
         // Add copy motor, motor follows other.
-
+        //FOllOWS?
+        shootLeft.follows(shootRight);
 
     }
-
+    //Sets Shooters Power in V
     public void setShooterPower(double power){
 
     }
-
+    //Set Shooters RPM (Possible?)
     public void setShooterRPM(){
 
     }
-
+    //Stops Shooter
     public void stopShooter(){
-        
+        shootRight.set(0);
+        shootLeft.set(0);
     }
-
+    //Returns current Shooter Power for both motors
+    public void CANSparkMax getShooterPower(){
+        return shootRight;
+        return shootLeft;
+    }
 
 }
