23797a54b99536a90843046fa5e9cf860a102aba
Ryan Alexander <ryandalex1@outlook.com>
2020-02-15 16:42:20 -0600

    Add a simple auto that will empty the hopper and then drive backwards 0.5 seconds

---
+++ src/main/java/com/team2502/robot2020/command/autonomous/groups/AutonCommandGroupFactory.java
@@ -0,0 +1,39 @@
+package com.team2502.robot2020.command.autonomous.groups;
+
+import com.team2502.robot2020.Constants;
+import com.team2502.robot2020.command.RunHopperContinuouslyCommand;
+import com.team2502.robot2020.command.ShootCommand;
+import com.team2502.robot2020.command.autonomous.ingredients.VoltageDriveCommand;
+import com.team2502.robot2020.subsystem.DrivetrainSubsystem;
+import com.team2502.robot2020.subsystem.HopperSubsystem;
+import com.team2502.robot2020.subsystem.ShooterSubsystem;
+import com.team2502.robot2020.subsystem.VisionSubsystem;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public class AutonCommandGroupFactory {
+    public static SequentialCommandGroup SimpleShoot3Balls(ShooterSubsystem SHOOTER,
+                                                           VisionSubsystem VISION,
+                                                           HopperSubsystem HOPPER,
+                                                           DrivetrainSubsystem DT) {
+        ParallelRaceGroup spoolUpShooter = new ParallelRaceGroup(
+                new ShootCommand(VISION, SHOOTER, Constants.OI.BUTTON_RUN_SHOOTER_INIT_LINE),
+                new WaitCommand(3)
+        );
+
+        ParallelRaceGroup runHopperAndShootBalls = new ParallelRaceGroup(
+                new ShootCommand(VISION, SHOOTER, Constants.OI.BUTTON_RUN_SHOOTER_INIT_LINE),
+                new RunHopperContinuouslyCommand(HOPPER),
+                new WaitCommand(5)
+        );
+
+        VoltageDriveCommand driveBackFromInitLine = new VoltageDriveCommand(DT, -1, -1, 0.5);
+
+        return new SequentialCommandGroup(
+                spoolUpShooter,
+                runHopperAndShootBalls,
+                driveBackFromInitLine
+        );
+    }
+}
---
+++ src/main/java/com/team2502/robot2020/command/autonomous/ingredients/VoltageDriveCommand.java
@@ -0,0 +1,43 @@
+package com.team2502.robot2020.command.autonomous.ingredients;
+
+import com.team2502.robot2020.subsystem.DrivetrainSubsystem;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class VoltageDriveCommand extends CommandBase {
+
+    private final DrivetrainSubsystem DT;
+    private final double left_volts;
+    private final double right_volts;
+    private final double timeout;
+    private final Timer timer;
+
+    public VoltageDriveCommand(DrivetrainSubsystem DT, double left_volts, double right_volts, double timeout) {
+        this.DT = DT;
+        this.left_volts = left_volts;
+        this.right_volts = right_volts;
+        this.timeout = timeout;
+
+        timer = new Timer();
+    }
+
+    @Override
+    public void initialize() {
+        timer.reset();
+    }
+
+    @Override
+    public void execute() {
+        DT.drive.tankDrive(left_volts, right_volts);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        DT.drive.tankDrive(0, 0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return timer.get() >= timeout;
+    }
+}
