880c3b08bb82263250f11ff4dc7778710a9e47f8
Ryan Alexander <ryandalex1@outlook.com>
2020-02-29 22:21:44 -0600

    Fixed turn to angle command (accidentally not committed before)

--- src/main/java/com/team2502/robot2020/command/autonomous/ingredients/TurnToAngleCommandNavX.java
+++ src/main/java/com/team2502/robot2020/command/autonomous/ingredients/TurnToAngleCommandNavX.java
@@ -9,15 +9,30 @@ import edu.wpi.first.wpilibj2.command.PIDCommand;
 public class TurnToAngleCommandNavX extends PIDCommand
 {
 
+    DrivetrainSubsystem drive;
+
     public TurnToAngleCommandNavX(DrivetrainSubsystem driveTrain, double targetDegrees){
         super(
-                new PIDController(0.02,0.0015,0),
+                new PIDController(0.02,0,0),
                 // Close loop on heading
                 driveTrain::getHeading,
                 // Set reference to target
                 targetDegrees,
                 // Pipe output to turn robot
-                output -> driveTrain.getDrive().arcadeDrive(0, -output),
+                output -> {
+                    double frictionConstant = Constants.Robot.Vision.FRICTION_LOW;
+                    double steering_power = 0;
+                    if (output > 0) {
+                        steering_power = output + frictionConstant;
+                    }
+                    else if (output < 0) {    //robot needs to turn left
+                        steering_power = output - frictionConstant;
+                    }
+
+                    double leftPower = -steering_power;
+                    double rightPower = steering_power;
+                    driveTrain.getDrive().tankDrive(leftPower, rightPower);
+                },
                 // Require the drive
                 driveTrain);
 
@@ -27,11 +42,18 @@ public class TurnToAngleCommandNavX extends PIDCommand
         // Set the controller tolerance - the delta tolerance ensures the robot is stationary at the
         // set point before it is considered as having reached the reference
         getController().setTolerance(Constants.Robot.Auto.TURN_TOLERANCE_DEG, Constants.Robot.Auto.TURN_RATE_TOLERANCE_DEG_PER_SEC);
+
+        drive = driveTrain;
     }
 
     @Override
     public boolean isFinished() {
         // End when the controller is at the reference.
         return getController().atSetpoint();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        //drive.getDrive().tankDrive(0,0);
     }
 }
